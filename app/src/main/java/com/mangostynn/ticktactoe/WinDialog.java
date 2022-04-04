f70a2425e15419a9acf8bb2780e0c89b05fcdcb0
rhonstratos <kizunaai20@gmail.com>
2022-04-05 01:24:57 +0800

    needs debugging

--- app/src/main/java/com/mangostynn/ticktactoe/WinDialog.java
+++ app/src/main/java/com/mangostynn/ticktactoe/WinDialog.java
@@ -3,6 +3,8 @@ package com.mangostynn.ticktactoe;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -18,6 +20,9 @@ public class WinDialog extends Dialog {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(  WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.win_dialog_layout);
 
         final TextView dialogMessage = findViewById(R.id.dialogMessage);
